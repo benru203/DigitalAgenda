24fb66726a3e68e4731967b6fa9c174e8fce4d6e
julian navarro <juln.navarro@gmail.com>
2019-03-19 19:40:56 -0500

    avance

---
+++ DigitalAgenda/app/src/main/java/com/example/digitalagenda/Data.java
@@ -0,0 +1,13 @@
+package com.example.digitalagenda;
+
+import java.util.ArrayList;
+
+public class Data {
+
+    private static ArrayList<Contact> Contacts = new ArrayList<>();
+
+    public static void Save(Contact c){Contacts.add(c);}
+
+    public static ArrayList<Contact> Get(){return Contacts;}
+
+}
--- DigitalAgenda/app/src/main/java/com/example/digitalagenda/MainActivity.java
+++ DigitalAgenda/app/src/main/java/com/example/digitalagenda/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.digitalagenda;
 
+import android.content.Intent;
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView LV;
+    private String Opc[];
+    private Resources Resources;
+    private Intent In;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
 }
